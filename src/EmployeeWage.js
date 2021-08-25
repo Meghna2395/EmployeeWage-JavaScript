9ff0a5a7ead13b11b7098908433362f6468f4114
Meghna2395 <meghnaborkar23@gmail.com>
2021-08-25 21:18:07 +0530

    UC2

--- src/EmployeeWage.js
+++ src/EmployeeWage.js
@@ -1,8 +1,19 @@
-const Is_Absent = 0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if(empCheck ==  Is_Absent){
-    console.log("Employee is Absent");
-    return;
-}else {
-    console.log("Employee is Present");
+const Is_PartTime = 1;
+const Is_FullTime = 2;
+const Part_Time_Hours = 4;
+const Full_Time_Hours = 8;
+const Wage_Per_Hour = 20;
+
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+switch(empCheck){
+    case Is_PartTime: 
+    empHrs = Part_Time_Hours;
+     case Is_FullTime:
+         empHrs = Full_Time_Hours;
+         break;
+         default:
+             empHrs = 0;
 }
+let empWage = empHrs * Wage_Per_Hour;
+console.log("Emp Wage:" + empWage);
