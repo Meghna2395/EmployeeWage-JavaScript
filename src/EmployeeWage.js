ffbc9d84226c90c064e088d6d195037bab0df013
Meghna2395 <meghnaborkar23@gmail.com>
2021-08-25 21:26:30 +0530

    UC3

--- src/EmployeeWage.js
+++ src/EmployeeWage.js
@@ -4,16 +4,18 @@ const Part_Time_Hours = 4;
 const Full_Time_Hours = 8;
 const Wage_Per_Hour = 20;
 
-let empHrs = 0;
-empCheck = Math.floor(Math.random() * 10) % 3;
-switch(empCheck){
-    case Is_PartTime: 
-    empHrs = Part_Time_Hours;
-     case Is_FullTime:
-         empHrs = Full_Time_Hours;
-         break;
-         default:
-             empHrs = 0;
+function getWorkingHours(empCheck){
+    switch (empCheck){
+        case Is_PartTime:
+            return Part_Time_Hours;
+        case Is_FullTime:
+            return Full_Time_Hours;
+            default:
+                return 0;    
+    }
 }
+let empHrs = 0;
+let empCheck = Math.floor(Math.random() * 10) % 3;
+empHrs = getWorkingHours(empCheck);
 let empWage = empHrs * Wage_Per_Hour;
 console.log("Emp Wage:" + empWage);
