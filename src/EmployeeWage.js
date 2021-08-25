c07a0bedb522c0030140fd39f1d07022ee517c7a
Meghna2395 <meghnaborkar23@gmail.com>
2021-08-25 21:49:20 +0530

    UC4

--- src/EmployeeWage.js
+++ src/EmployeeWage.js
@@ -4,18 +4,25 @@ const Part_Time_Hours = 4;
 const Full_Time_Hours = 8;
 const Wage_Per_Hour = 20;
 
-function getWorkingHours(empCheck){
-    switch (empCheck){
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
         case Is_PartTime:
             return Part_Time_Hours;
         case Is_FullTime:
             return Full_Time_Hours;
-            default:
-                return 0;    
+        default:
+            return 0;
     }
 }
+
+
+
+const Num_of_WorkingDays = 2;
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-empHrs = getWorkingHours(empCheck);
+for (let day = 0; day < Num_of_WorkingDays; day++) {
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    empHrs += getWorkingHours(empCheck);
+}
+
 let empWage = empHrs * Wage_Per_Hour;
-console.log("Emp Wage:" + empWage);
+console.log("Total Hrs:" + empHrs + "Emp Wage:" + empWage);
